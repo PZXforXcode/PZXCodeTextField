7db9b94613e122a5b03d2c5d6c32032b5627e6a5
pzx <pengzuxinXcode@outlook.com>
2024-08-16 10:53:53 +0800

    添加SPM

---
+++ PZXVerificationCodeTextField/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PZXVerificationCodeTextField",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PZXVerificationCodeTextField",
+            targets: ["PZXVerificationCodeTextField"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PZXVerificationCodeTextField",
+            path: "Sources",
+            exclude: [
+                "PZXVerificationCodeTextField-OC"
+            ]
+        )
+    ]
+)
